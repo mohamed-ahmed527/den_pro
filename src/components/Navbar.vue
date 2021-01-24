15a75dfd5792215f229f456efe34f5bbbfb189e4
mohamed-ahmed527 <71071161+mohamed-ahmed527@users.noreply.github.com>
2021-01-25 00:34:49 +0200

    Update Navbar.vue

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -96,6 +96,7 @@ export default {
                 li{
                     width: 100%;
                     margin-bottom: 5px;
+                    text-align:left;
                 }
             }
         }
